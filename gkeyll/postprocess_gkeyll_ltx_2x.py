e429af281a70c5d3e47f69876bc54dccf76e7366
Manaure Francisquez <mfrancisquez@gmail.com>
2026-07-14 10:32:31 -0700

    Correct electron tperp tpar plots, add temp plots.

--- gkeyll/postprocess_gkeyll_ltx_2x.py
+++ gkeyll/postprocess_gkeyll_ltx_2x.py
@@ -705,17 +705,18 @@ if plot_vs_RZ:
 
   data_dir = '/pscratch/sd/m/mana/gkeyll/ltx/2d/li863mg_103955_04-base/'
 
-  quant      = 'elc_BiMaxwellianMoments' #[ Quantity to plot.
-  quant_comp = 'den'                    #[ Component in file (den, upar, tpar, tperp, temp, or an int).
-  scale_fac  = 1.0               #[ Factor to multiply data by.
-#  scale_fac  = lcu.mass_ion/lcu.eV               #[ Factor to multiply data by.
-  zlabel     = r'$n_e(t=0.7~\mathrm{ms})$ (m$^{-3}$)'       #[ Label for y axis.
+  quant      = 'ion_BiMaxwellianMoments' #[ Quantity to plot.
+  quant_comp = 'temp'                    #[ Component in file (den, upar, tpar, tperp, temp, or an int).
+#  scale_fac  = 1.0               #[ Factor to multiply data by.
+  scale_fac  = lcu.mass_ion/lcu.eV               #[ Factor to multiply data by.
+#  zlabel     = r'$n_e(t=0.7~\mathrm{ms})$ (m$^{-3}$)'       #[ Label for y axis.
 #  zlabel     = r'$u_{\parallel i}(t=0.7~\mathrm{ms})$ (km/s)'       #[ Label for y axis.
 #  zlabel     = r'$T_{\parallel e}(t=0.7~\mathrm{ms})$ (eV)'       #[ Label for y axis.
 #  zlabel     = r'$T_{\perp e}(t=0.7~\mathrm{ms})$ (eV)'       #[ Label for y axis.
+  zlabel     = r'$T_{i}(t=0.7~\mathrm{ms})$ (eV)'       #[ Label for y axis.
   frame      = 14                         #[ Frame number.
 
-  fig_file_name_root = lcu.li863_prefix+'final_elc_den_RZ'
+  fig_file_name_root = lcu.li863_prefix+'final_ion_temp_RZ'
 
   wall_file = '/global/homes/m/mana/perlmutter/gkeyll/code/gkyl-sims/ltx_gkeyll_xgc/experiment/LTXvessel.csv'
 
